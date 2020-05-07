cf883ec2a250fa35fecffe4b1681ba31f0642b68
Misha Pozdniakov <misha.pozd@gmail.com>
2020-05-07 17:13:25 -0400

    added css styling and strikethrough capability onToggle and onDelete functions also introduced

--- angular-todolist/src/app/components/todo-item/todo-item.component.ts
+++ angular-todolist/src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo'
 
 @Component({
   selector: 'app-todo-item',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //Set Dynamic Classes
+  setClasses(){
+    let classes ={
+      todo:true,
+      'is-complete':this.todo.completed
+    }
+    return classes
+  }
+
+  onToggle(todo){
+    console.log('toggle');
+  }
+
+  onDelete(todo){
+    console.log('delete');
   }
 
 }
