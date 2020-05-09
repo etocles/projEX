bac4d31b30da386adc2a5523ba707f3f6752c434
Misha Pozdniakov <misha.pozd@gmail.com>
2020-05-09 02:37:32 -0400

    changed todos to custom made classes w/o constructors

--- angular-todolist/src/app/components/todos/todos.component.ts
+++ angular-todolist/src/app/components/todos/todos.component.ts
@@ -38,243 +38,243 @@ export class TodosComponent implements OnInit {
     // ];
 
 
-    // this.todos = [
-    //   //First Project
-    //   {
-    //     name:'ProjectName',
-    //     category:'CategoryName',
-    //     due_date: new Date('2020-05-09T23:59:00'),
-    //     order_matters:false,
-    //     completed:false,
-    //     progbar:{
-    //       num_done:3,
-    //       benchmarks:[
-    //         {
-    //           id:1,
-    //           title:"Bench1",
-    //           due_date: new Date('2019-02-18T23:59:00'),
-    //           completed:true,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:2,
-    //           title:"Bench2",
-    //           due_date: new Date('2019-02-19T23:59:00'),
-    //           completed:true,
-    //           isnested:true,
-    //           nested_bar:{
-    //             miniBar:[
-    //               {
-    //                 id:1,
-    //                 name:"a",
-    //                 completed:true
-    //               },
-    //               {
-    //                 id:2,
-    //                 name:"b",
-    //                 completed:true
-    //               },
-    //               {
-    //                 id:3,
-    //                 name:"c",
-    //                 completed:true
-    //               }
-    //             ]
-    //           }
-    //         },
-    //         {
-    //           id:3,
-    //           title:"Bench3",
-    //           due_date: new Date('2019-02-20T23:59:00'),
-    //           completed:true,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:4,
-    //           title:"Bench4",
-    //           due_date: new Date('2019-02-21T23:59:00'),
-    //           completed:false,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:5,
-    //           title:"Bench5",
-    //           due_date: new Date('2019-02-22T23:59:00'),
-    //           completed:false,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //       ]
-    //     }
-    //   },
-    //   //Second Project
-    //   {
-    //     name:'ProjectName2',
-    //     category:'CategoryName2',
-    //     due_date: new Date('2020-06-09T23:59:00'),
-    //     order_matters:false,
-    //     completed:false,
-    //     progbar:{
-    //       num_done:1,
-    //       benchmarks:[
-    //         {
-    //           id:1,
-    //           title:"Bench2",
-    //           due_date: new Date('2019-02-18T23:59:00'),
-    //           completed:true,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:2,
-    //           title:"Bench2",
-    //           due_date: new Date('2019-02-18T23:59:00'),
-    //           completed:false,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:3,
-    //           title:"Bench3",
-    //           due_date: new Date('2019-02-20T23:59:00'),
-    //           completed:false,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:4,
-    //           title:"Bench4",
-    //           due_date: new Date('2019-02-21T23:59:00'),
-    //           completed:false,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:5,
-    //           title:"Bench5",
-    //           due_date: new Date('2019-02-22T23:59:00'),
-    //           completed:false,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //       ]
-    //     }
-    //   },
+    this.todos = [
+      //First Project
+      {
+        name:'ProjectName',
+        category:'CategoryName',
+        due_date: new Date('2020-05-09T23:59:00'),
+        order_matters:false,
+        completed:false,
+        progbar:{
+          num_done:3,
+          benchmarks:[
+            {
+              id:1,
+              title:"Bench1",
+              due_date: new Date('2019-02-18T23:59:00'),
+              completed:true,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:2,
+              title:"Bench2",
+              due_date: new Date('2019-02-19T23:59:00'),
+              completed:true,
+              isnested:true,
+              nested_bar:{
+                miniBar:[
+                  {
+                    id:1,
+                    name:"a",
+                    completed:true
+                  },
+                  {
+                    id:2,
+                    name:"b",
+                    completed:true
+                  },
+                  {
+                    id:3,
+                    name:"c",
+                    completed:true
+                  }
+                ]
+              }
+            },
+            {
+              id:3,
+              title:"Bench3",
+              due_date: new Date('2019-02-20T23:59:00'),
+              completed:true,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:4,
+              title:"Bench4",
+              due_date: new Date('2019-02-21T23:59:00'),
+              completed:false,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:5,
+              title:"Bench5",
+              due_date: new Date('2019-02-22T23:59:00'),
+              completed:false,
+              isnested:false,
+              nested_bar:null
+            },
+          ]
+        }
+      },
+      //Second Project
+      {
+        name:'ProjectName2',
+        category:'CategoryName2',
+        due_date: new Date('2020-06-09T23:59:00'),
+        order_matters:false,
+        completed:false,
+        progbar:{
+          num_done:1,
+          benchmarks:[
+            {
+              id:1,
+              title:"Bench2",
+              due_date: new Date('2019-02-18T23:59:00'),
+              completed:true,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:2,
+              title:"Bench2",
+              due_date: new Date('2019-02-18T23:59:00'),
+              completed:false,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:3,
+              title:"Bench3",
+              due_date: new Date('2019-02-20T23:59:00'),
+              completed:false,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:4,
+              title:"Bench4",
+              due_date: new Date('2019-02-21T23:59:00'),
+              completed:false,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:5,
+              title:"Bench5",
+              due_date: new Date('2019-02-22T23:59:00'),
+              completed:false,
+              isnested:false,
+              nested_bar:null
+            },
+          ]
+        }
+      },
+
+      //project 3
+      {
+        name:'ProjectName3',
+        category:'CategoryName3',
+        due_date: new Date('2020-11-09T23:59:00'),
+        order_matters:false,
+        completed:false,
+        progbar:{
+          num_done:4,
+          benchmarks:[
+            {
+              id:1,
+              title:"Bench2",
+              due_date: new Date('2019-02-18T23:59:00'),
+              completed:true,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:2,
+              title:"Bench2",
+              due_date: new Date('2019-02-18T23:59:00'),
+              completed:true,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:3,
+              title:"Bench3",
+              due_date: new Date('2019-02-20T23:59:00'),
+              completed:true,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:4,
+              title:"Bench4",
+              due_date: new Date('2019-02-21T23:59:00'),
+              completed:true,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:5,
+              title:"Bench5",
+              due_date: new Date('2019-02-22T23:59:00'),
+              completed:false,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:6,
+              title:"Bench6",
+              due_date: new Date('2019-02-22T23:59:00'),
+              completed:false,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:7,
+              title:"Bench7",
+              due_date: new Date('2019-02-22T23:59:00'),
+              completed:false,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:8,
+              title:"Bench8",
+              due_date: new Date('2019-02-22T23:59:00'),
+              completed:false,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:9,
+              title:"Bench9",
+              due_date: new Date('2019-02-22T23:59:00'),
+              completed:false,
+              isnested:false,
+              nested_bar:null
+            },
+            {
+              id:10,
+              title:"Bench10",
+              due_date: new Date('2019-02-22T23:59:00'),
+              completed:false,
+              isnested:false,
+              nested_bar:null
+            },
+          ]
+        }
+      }
+    ]
+
+
+
+    // //some debugging
+    // let p = new ProgressBar;
+    // // console.log(p.num_done);
+    // // console.log(p.benchmarks.length);
+    // // console.log(p.benchmarks[1].title);
     //
-    //   //project 3
-    //   {
-    //     name:'ProjectName3',
-    //     category:'CategoryName3',
-    //     due_date: new Date('2020-11-09T23:59:00'),
-    //     order_matters:false,
-    //     completed:false,
-    //     progbar:{
-    //       num_done:4,
-    //       benchmarks:[
-    //         {
-    //           id:1,
-    //           title:"Bench2",
-    //           due_date: new Date('2019-02-18T23:59:00'),
-    //           completed:true,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:2,
-    //           title:"Bench2",
-    //           due_date: new Date('2019-02-18T23:59:00'),
-    //           completed:true,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:3,
-    //           title:"Bench3",
-    //           due_date: new Date('2019-02-20T23:59:00'),
-    //           completed:true,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:4,
-    //           title:"Bench4",
-    //           due_date: new Date('2019-02-21T23:59:00'),
-    //           completed:true,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:5,
-    //           title:"Bench5",
-    //           due_date: new Date('2019-02-22T23:59:00'),
-    //           completed:false,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:6,
-    //           title:"Bench6",
-    //           due_date: new Date('2019-02-22T23:59:00'),
-    //           completed:false,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:7,
-    //           title:"Bench7",
-    //           due_date: new Date('2019-02-22T23:59:00'),
-    //           completed:false,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:8,
-    //           title:"Bench8",
-    //           due_date: new Date('2019-02-22T23:59:00'),
-    //           completed:false,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:9,
-    //           title:"Bench9",
-    //           due_date: new Date('2019-02-22T23:59:00'),
-    //           completed:false,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //         {
-    //           id:10,
-    //           title:"Bench10",
-    //           due_date: new Date('2019-02-22T23:59:00'),
-    //           completed:false,
-    //           isnested:false,
-    //           nested_bar:null
-    //         },
-    //       ]
-    //     }
-    //   }
-    // ]
-
-
-
-    //some debugging
-    let p = new ProgressBar;
-    // console.log(p.num_done);
-    // console.log(p.benchmarks.length);
-    // console.log(p.benchmarks[1].title);
-
-    let proj = new Project;
-    proj.progbar = p;
-    console.log(proj instanceof Project);
-    console.log(proj.progbar instanceof ProgressBar);
-
-
-    //using constructors
-    this.todos =[proj,proj,proj];
+    // let proj = new Project;
+    // proj.progbar = p;
+    // console.log(proj instanceof Project);
+    // console.log(proj.progbar instanceof ProgressBar);
+    //
+    //
+    // //using constructors
+    // this.todos =[proj,proj,proj];
   }
 
   deleteTodo(todo:Project){
