505c70a43361cef142583d0ee6c155488681abbf
Misha Pozdniakov <misha.pozd@gmail.com>
2020-05-15 16:38:28 -0400

    progressbars dynamically change color!!

--- angular-todolist/src/app/components/benchmark/benchmark.component.ts
+++ angular-todolist/src/app/components/benchmark/benchmark.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from "@angular/common";
-import { Bench, NestedBar } from 'src/app/models/Todo';
+import { ProgressBar, Bench, NestedBar } from 'src/app/models/Todo';
 
 @Component({
   selector: 'app-benchmark',
@@ -9,19 +9,24 @@ import { Bench, NestedBar } from 'src/app/models/Todo';
 })
 export class BenchmarkComponent implements OnInit {
   @Input() bench: Bench;
+  @Input() prog:ProgressBar;
   @Output() markUpTo: EventEmitter<Bench> = new EventEmitter();
   @Output() markDownTo: EventEmitter<Bench> = new EventEmitter();
 
   nested:boolean;
   nested_bar:NestedBar;
+  percent:number;
 
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.nested = this.bench.isnested;
     if(this.nested){
       this.nested_bar=this.bench.nested_bar;
     }
+    this.percent = this.prog.num_done/this.prog.benchmarks.length;
   }
 
   setClasses(){
@@ -44,7 +49,43 @@ export class BenchmarkComponent implements OnInit {
   nestedMark(b:Bench){
     if (b.completed == true)this.markUpTo.emit(this.bench);
     else this.markDownTo.emit(this.bench);
+  }
 
+  getStyles(){
+    this.percent = this.prog.num_done/this.prog.benchmarks.length;
+    let h = 130*this.percent; //maybe 10+120*this.percent instead
+    let s = 86;
+    let l = 56;
+    h /= 360;
+    s /= 100;
+    l /= 100;
+    let r, g, b;
+    if (s === 0) {
+      r = g = b = l; // achromatic
+    } else {
+      const hue2rgb = (p, q, t) => {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      };
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
+    const toHex = x => {
+      const hex = Math.round(x * 255).toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+    };
+    let progColor = "#"+toHex(r)+toHex(g)+toHex(b);
+    let linGradientText = 'linear-gradient(to left, '+ `${progColor}` +' 50%, gray 50%)'
+    return{
+      'backgroundImage': linGradientText
+    };
   }
 
 }
