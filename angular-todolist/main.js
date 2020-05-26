906c92a49acda238a2304b8769b9a7d0138ae45d
Misha Pozdniakov <misha.pozd@gmail.com>
2020-05-26 13:58:49 -0400

    WORKING ELECTRON VERSION

---
+++ angular-todolist/main.js
@@ -0,0 +1,41 @@
+const {app, BrowserWindow, screen} = require('electron')
+const url = require("url");
+const path = require("path");
+
+let mainWindow
+
+function createWindow () {
+  const size = screen.getPrimaryDisplay().workAreaSize;
+  mainWindow = new BrowserWindow({
+    width: size.width,
+    height: size.height*.7,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
